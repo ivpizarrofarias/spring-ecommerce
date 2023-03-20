a7c973d74cc3c02ae8412266fbb46c6b2ff41181
ivpizarrofarias <ivpizarrofarias@gmail.com>
2023-03-20 01:21:50 -0300

    subida de archivos

---
+++ src/test/java/com/ipf/Software/SpringEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipf.Software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
